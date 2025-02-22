a80e3db4134c885f4a4863784e2c0059bb11df48
Adrienne Shayne Buac <asmbuac@gmail.com>
2025-02-21 22:33:40 -0800

    feat: use fetch favicon, add custom tw colors, & upload assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {
+        'fetch-primary': '#300d38',
+        'fetch-secondary': '#890a74',
+        'fetch-tertiary': '#ffa900',
+        'fetch-success': '#04833e',
+        'fetch-danger': '#dc3545',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
